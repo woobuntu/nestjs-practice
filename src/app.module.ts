6132741dbb4a6c186cfd78b15b2616424d6f29a2
woobuntu <boutime2017@gmail.com>
2021-07-20 09:52:50 +0900

    nest g module 명령어를 통한 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
 
 // @Module은 AppModule 클래스를 모듈로 변환하는 데코레이터이다.
 @Module({
-  imports: [],
+  imports: [TasksModule],
 })
 export class AppModule {}
