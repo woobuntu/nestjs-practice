6e19f22bf973171aa280e2484e0a6e9822062a52
woobuntu <boutime2017@gmail.com>
2021-07-21 08:41:28 +0900

    nest cli를 통한 service 생성 및 controller에 service dependency injection

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+  // 이렇게 접근자를 사용하여 클래스의 프로퍼티를 정의하는 것은 타입스크립트에서만 지원하는 기능이다.
+}
