df17c82ef1af1b87848b2cd606a4c195ac57e3ea
woobuntu <boutime2017@gmail.com>
2021-07-21 10:11:30 +0900

    getAll메서드 구현

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = [];
+
+  // 타입스크립트에서 메소드를 정의할 때 명시적으로 접근자를 선언해주지 않으면 디폴트로 public이 된다.
+  getAllTasks() {
+    return this.tasks;
+  }
+}
