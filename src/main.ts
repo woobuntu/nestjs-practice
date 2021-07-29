6bec9f423577d4babc5c3185f90f8ea1e8aedb21
in uh, lee <boutime2017@gmail.com>
2021-07-29 10:58:40 +0900

    fastify 버전에 맞게 수정

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Module } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
 @Controller()
 class AppController {
   @Get()
@@ -14,8 +17,11 @@ class AppController {
 class AppModule {}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true })
+  );
 
-  await app.listen(3000);
+  await app.listen(3000, "0.0.0.0");
 }
 bootstrap();
