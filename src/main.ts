6b512158d59197810c8eb6c521687bbdd64c48b8
in uh, lee <boutime2017@gmail.com>
2021-08-01 17:03:03 +0900

    mater브랜치에 있는 내용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module'; // root 모듈
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  // validation 관련 decorator를 만나면 validationPipe를 실행한다는 것
   await app.listen(3000);
 }
 bootstrap();
