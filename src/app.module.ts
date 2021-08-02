1daab4494a98553379bebff7fc3a2ae13ea050cf
in uh, lee <boutime2017@gmail.com>
2021-08-02 15:21:06 +0900

    database connection 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
 
 // @Module은 AppModule 클래스를 모듈로 변환하는 데코레이터이다.
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    ReportsModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
