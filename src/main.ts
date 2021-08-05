7e672f9511dcf2b145a4440e1135f09074824ef9
in uh, lee <boutime2017@gmail.com>
2021-08-05 14:41:41 +0900

    애플리케이션 레벨 validation 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module'; // root 모듈
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
